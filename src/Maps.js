242310d5722d1f0a1d5cf657e58208e1751679cc
Arjun Attam <arjunattam@gmail.com>
2020-03-25 20:35:46 -0700

    maps

---
+++ src/Maps.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onMapClicked = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Map google={this.props.google} onClick={this.onMapClicked}>
+        <Marker onClick={this.onMarkerClick} name={"Current location"} />
+
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name}</h1>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+const GOOGLE_MAPS_API_KEY = "AIzaSyB9hwI7b4677POloj5DpmDXaliqU5Dp8sA";
+
+export default GoogleApiWrapper({
+  apiKey: GOOGLE_MAPS_API_KEY
+})(MapContainer);

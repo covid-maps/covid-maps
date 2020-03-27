915614d42b2cf755e2ad8076bc38020193581626
Arjun Attam <arjunattam@gmail.com>
2020-03-27 14:13:04 -0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -52,6 +52,14 @@ async function addRow(values) {
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("hello, world!");

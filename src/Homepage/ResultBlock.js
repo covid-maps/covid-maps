a437decae3e37ac3cac8b9ff6703abf21e51b317
Arjun Attam <arjunattam@gmail.com>
2020-03-27 23:38:59 -0700

    revert result block

--- src/Homepage/ResultBlock.js
+++ src/Homepage/ResultBlock.js
@@ -4,29 +4,15 @@ import { Link } from "react-router-dom";
 function ResultBlock(props) {
   return (
     <div className="my-3">
-      <h5>{props["Store Name"]}</h5>
+      <h5>{props.Name}</h5>
       <h6>{props["Manual Address"]}</h6>
+      <div>
+        <span>{props.Notes}</span> <strong>{props.Timestamp}</strong>
+      </div>
       <div>
         <span>
           <Link to="/submit">Share your experience</Link>
         </span>
-      </div>
-      <div class="card">
-        <div class="card-header">
-          <strong>{props["Timestamp"]}</strong>
-        </div>
-        <div class="card-body">
-          <table class="table">
-            <tr>
-              <th scope="row">Notes</th>
-              <td>{props["Useful Information"]}</td>
-            </tr>
-            <tr>
-              <th scope="row">Safety</th>
-              <td>{props["Safety"]}</td>
-            </tr>
-          </table>
-        </div>
       </div>
     </div>
   );

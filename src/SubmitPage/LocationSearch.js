89491fac8dba4d9265402ea6baf7b2da576376c4
Arjun Attam <arjunattam@gmail.com>
2020-03-27 20:11:56 -0700

    remove typehead lib

--- src/SubmitPage/LocationSearch.js
+++ src/SubmitPage/LocationSearch.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import { Typeahead } from "react-bootstrap-typeahead";
-import "react-bootstrap-typeahead/css/Typeahead.css";
 import PlacesAutocomplete, {
   geocodeByAddress,
   getLatLng

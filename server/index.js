e8fbf2fb36fc4c0b77e6439094b36d5312d0de31
Arjun Attam <arjunattam@gmail.com>
2020-03-27 20:24:16 -0700

    use new db

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const GOOGLE_SERVICE_ACCOUNT_EMAIL =
   "toilet-paper-app@eco-theater-119616.iam.gserviceaccount.com";
 
 const doc = new GoogleSpreadsheet(
-  "1dWBcbwapI4ZtlbJD257b3v8wjTfFECwLl5W_Y_DYVws"
+  "1jFQrYwbhPIaRL6t4TnpTO7W905U0B-W1FXS-GBYwz7M"
 );
 
 async function authenticate() {

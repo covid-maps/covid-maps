3ab4100777c4eff889b77200959558c2573745ec
Ari Purcell <arrian.purcell@gmail.com>
2020-03-28 18:20:55 -0700

    Show homepage markers.

--- src/Homepage/SearchResults.js
+++ src/Homepage/SearchResults.js
@@ -1,27 +1,12 @@
 import React from "react";
 import ResultBlock from "./ResultBlock";
-import * as api from "../api";
 
 class SearchResults extends React.Component {
-  state = {
-    results: [],
-    isLoading: true
-  };
-
-  componentDidMount() {
-    api.query().then(data => {
-      this.setState({
-        results: data,
-        isLoading: false
-      });
-    });
-  }
-
   render() {
-    return this.state.isLoading ? (
+    return this.props.isLoading ? (
       <div>Loading...</div>
     ) : (
-      this.state.results.map(result => (
+      this.props.results.map(result => (
         <ResultBlock {...result} key={result.Timestamp} />
       ))
     );

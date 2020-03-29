06fe1276ee09ed8b06b90cf21ed69c2fd6309f8e
Sampoorna Biswas <sampoorna@clir.eco>
2020-03-28 18:28:25 -0700

    Only show store name if there is no value in the Useful Information fields

--- src/Homepage/Result.js
+++ src/Homepage/Result.js
@@ -15,15 +15,22 @@ function Timestamp({ Timestamp: value }) {
 }
 
 function Result(storeResults) {
+  const resultList = []
+  for (const key in storeResults) {
+    if (storeResults[key]["Useful Information"])
+      {
+        resultList.push(storeResults[key]);
+      }
+  }
   return (
-    Object.keys(storeResults).map(resultIndex => (
+    resultList.map(result => (
       <div className="my-3">
         <div>
-          <span>{storeResults[resultIndex]["Useful Information"]}</span> <Timestamp {...storeResults[resultIndex]} />
+          <span>{result["Useful Information"]}</span> <Timestamp {...result} />
         </div>
         <div>
           <span>
-            <Link to={{ pathname: "/submit", state: { item: storeResults[resultIndex] } }}>
+            <Link to={{ pathname: "/submit", state: { item: result } }}>
             Update this information
             </Link>
           </span>

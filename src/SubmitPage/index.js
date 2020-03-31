eaec9cfa8819b7864aaa5da48c41cabbb60e76ea
asanjeevrao <39347351+asanjeevrao@users.noreply.github.com>
2020-03-30 18:55:37 -0700

    testing GA page event

--- src/SubmitPage/index.js
+++ src/SubmitPage/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import SubmitForm from "./SubmitForm";
 import { ScrollToTopOnMount } from "../utils";
+import ReactGA from "react-ga";
 
 export default function SubmitPage(props) {
+  ReactGA.pageview("/Update");
   return (
     <div>
       <ScrollToTopOnMount />
       <SubmitForm {...props} />
     </div>
+    
   );
 }

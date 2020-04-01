1f5b737488b7e98b9fdef5e6760148d699446d61
Arjun Attam <arjunattam@gmail.com>
2020-03-31 18:10:54 -0700

    perf

--- server/index.js
+++ server/index.js
@@ -1,12 +1,7 @@
 const express = require("express");
+const compression = require("compression");
 const bodyParser = require("body-parser");
 const { GoogleSpreadsheet } = require("google-spreadsheet");
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
 
 const GOOGLE_SERVICE_ACCOUNT_EMAIL =
   "toilet-paper-app@eco-theater-119616.iam.gserviceaccount.com";
@@ -55,6 +50,7 @@ async function addRow(values) {
 
 const app = express();
 app.use(express.json());
+app.use(compression());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -68,19 +64,6 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.send("hello, world!");
 });
-
-app.get('/db', async (req, res) => {
-  try {
-    const client = await pool.connect()
-    const result = await client.query('SELECT * FROM covid-maps-app.activeservices');
-    const results = { 'results': (result) ? result.rows : null};
-    res.render('pages/db', results );
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-})
 
 app.get("/v0/query", async (req, res) => {
   try {

67190d185330fc0603cd352e826e05ba855c07a7
Arjun Attam <arjunattam@gmail.com>
2020-04-03 23:19:49 -0700

    updated logo and about page

--- src/AboutPage/index.js
+++ src/AboutPage/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faTimes, faWhatsappSquare } from "@fortawesome/free-solid-svg-icons";
 
 function NewTabLink({ href, children }) {
   return (
@@ -13,29 +16,44 @@ function AboutPage() {
   return (
     <div className="container p-4 about-page">
       <h4 className="text-uppercase mb-1">About</h4>
-      <p className="text-black-50">
+      <p className="text-black-60">
         Covid Maps is a crowd-sourced app that helps you track the latest
         information on essential services operating around you during the
         COVID-19 shutdowns. <Link to="/update">Add an update</Link> about a
         store near you and help others!
       </p>
-      <p className="text-black-50">
+      <p className="text-black-60">
         We are a small team of volunteers in Bengaluru, San Francisco and
         Vancouver.
       </p>
       <h4 className="mt-4 text-uppercase mb-1">Contribute</h4>
-      <p className="text-black-50">
-        We are looking for volunteers that can help with operations and
-        technology. Join our{" "}
-        <NewTabLink href="https://t.me/covidmaps">Telegram group</NewTabLink> or
-        find us on{" "}
-        <NewTabLink href="https://github.com/arjun27/covid-maps">
-          GitHub
-        </NewTabLink>
-        .
+      <p className="text-black-60">
+        We are looking for volunteers that can help with technology and
+        outreach.
+      </p>
+      <p>
+        <a href="https://github.com/covid-maps/covid-maps">
+          <Button variant="info">
+            <i className="fab fa-github" /> Join us on GitHub
+          </Button>
+        </a>
+      </p>
+      <p>
+        <a href="https://chat.whatsapp.com/HzZT0gMYoYYEDDjj2LQUD8">
+          <Button variant="info">
+            <i className="fab fa-whatsapp" /> Join our Developer group
+          </Button>
+        </a>
+      </p>
+      <p>
+        <a href="https://chat.whatsapp.com/BXh2FuYjW55Ee26xuzoWcw">
+          <Button variant="info">
+            <i className="fab fa-whatsapp" /> Join our Outreach group
+          </Button>
+        </a>
       </p>
       <h4 className="mt-4 text-uppercase mb-1">Privacy Policy</h4>
-      <p className="text-black-50">
+      <p className="text-black-60">
         We collect your IP address when you update information on a store. This
         helps us identify the authenticity of the submission. No other personal
         information is collected.

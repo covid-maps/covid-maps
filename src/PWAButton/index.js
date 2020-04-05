05adcd30fbb5037ac8ef0af8abdc88e3613e939d
Arjun Attam <arjunattam@gmail.com>
2020-04-05 14:32:20 -0700

    more fixes

--- src/PWAButton/index.js
+++ src/PWAButton/index.js
@@ -20,7 +20,7 @@ function PWAInstallButton() {
   // TODO: don't show if the app is already installed
   // Adds the `deferredPrompt` object to the window.
   window.addEventListener("beforeinstallprompt", event => {
-    setShowInstall(false);
+    // setShowInstall(false);
     event.preventDefault();
     window.deferredPrompt = event;
   });

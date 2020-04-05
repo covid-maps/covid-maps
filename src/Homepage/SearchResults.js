d3a0b539741e634686cfab471fb6c80616ff14d0
mittal201 <anshul.mittal@tradefin.com>
2020-04-05 10:44:17 +0530

    add pagination for filtered results based on location

--- src/Homepage/SearchResults.js
+++ src/Homepage/SearchResults.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ResultBlock from "./ResultBlock";
 import { isSameLocation } from "../utils";
+import Pagination from "react-js-pagination";
+
 
 class SearchResults extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePage: 1,
+            itemPerPage: 10
+        };
+    }
+
+    handlePageChange(pageNumber) {
+        console.log(`active page is ${pageNumber}`);
+        this.setState({activePage: pageNumber});
+    }
+
   render() {
     const selectedResult = this.props.results.find(result =>
       isSameLocation(result, this.props.selectedLocation)
@@ -11,6 +26,9 @@ class SearchResults extends React.Component {
       result => !isSameLocation(result, this.props.selectedLocation)
     );
     const results = selectedResult ? [selectedResult, ...filtered] : filtered;
+      let indexOfLastResult = this.state.activePage * this.state.itemPerPage;
+      let indexOfFirstResult = indexOfLastResult - this.state.itemPerPage;
+      let renderedResults = results.slice(indexOfFirstResult, indexOfLastResult);
     return this.props.isLoading ? (
       <div className="text-center py-5">
         <div className="spinner-border text-secondary" role="status">
@@ -18,7 +36,8 @@ class SearchResults extends React.Component {
         </div>
       </div>
     ) : (
-      results.map(result => {
+      <div>{
+          renderedResults.map(result => {
         return (
           <div key={result.placeId || result.name}>
             <ResultBlock
@@ -27,7 +46,17 @@ class SearchResults extends React.Component {
             />
           </div>
         );
-      })
+      })}
+          <Pagination
+              itemClass="page-item"
+              linkClass="page-link"
+              activePage={this.state.activePage}
+              itemsCountPerPage={this.state.itemPerPage}
+              totalItemsCount={results.length}
+              pageRangeDisplayed={5}
+              onChange={this.handlePageChange.bind(this)}
+          />
+      </div>
     );
   }
 }

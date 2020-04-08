e783e9e7a07258f3564b711e8d1136e72fd70ade
siddharth11 <siddharth2parmar@gmail.com>
2020-04-08 18:26:53 +0200

    Add language selector dropdown in nav

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component, createContext } from "react";
 import { Router, Switch, Route, Link } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import { Dropdown } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Homepage from "./Homepage";
@@ -12,6 +13,8 @@ import Navbar from "react-bootstrap/Navbar";
 import PWAInstallButton from "./PWAButton";
 import ReactGA from "react-ga";
 import logo from "./Logo.svg";
+import { AVAILABLE_LANGUAGES } from "./constants";
+import translations from "./translations";
 
 const history = createBrowserHistory();
 if (process.env.NODE_ENV !== "development") {
@@ -23,7 +26,16 @@ if (process.env.NODE_ENV !== "development") {
   });
 }
 
-function AppNavbar() {
+const IconToggle = React.forwardRef(({ onClick }, ref) => (
+  <i
+    ref={ref}
+    onClick={onClick}
+    tabIndex={0}
+    className="far fa-language language_icon"
+  />
+));
+
+const AppNavbar = ({ setLanguage, currentLanguage }) => {
   return (
     <>
       <Navbar bg="light" variant="light" fixed="top">
@@ -37,35 +49,95 @@ function AppNavbar() {
             />{" "}
           </Link>
         </Navbar.Brand>
-        <PWAInstallButton />
+        <Dropdown className="dropdown--translation">
+          <Dropdown.Toggle as={IconToggle} />
+          <Dropdown.Menu>
+            {Object.keys(AVAILABLE_LANGUAGES).map(key => {
+              const langValue = AVAILABLE_LANGUAGES[key];
+              return (
+                <Dropdown.Item
+                  key={langValue}
+                  active={currentLanguage === langValue}
+                  onClick={() => setLanguage(langValue)}
+                >
+                  {langValue}
+                </Dropdown.Item>
+              );
+            })}
+          </Dropdown.Menu>
+        </Dropdown>
+        {/* <PWAInstallButton /> */}
       </Navbar>
     </>
   );
+};
+
+const AppContext = createContext();
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      language: AVAILABLE_LANGUAGES.ENGLISH
+    };
+  }
+
+  setLanguage = language => {
+    this.setState({ language });
+  };
+
+  getTranslations = () => {
+    return {
+      ...translations[AVAILABLE_LANGUAGES.ENGLISH],
+      ...translations[this.state.language]
+    };
+  };
+
+  render() {
+    return (
+      <Router history={history}>
+        <AppContext.Provider
+          value={{
+            translations: this.getTranslations()
+          }}
+        >
+          <div className="App">
+            <ScrollToTop />
+            <AppNavbar
+              currentLanguage={this.state.language}
+              setLanguage={this.setLanguage}
+            />
+            <div className="page">
+              <Switch>
+                <Route path="/update" component={SubmitPage} />
+                <Route path="/location" component={LocationSelectionPage} />
+                <Route path="/about" component={AboutPage} />
+                <Route path="/" component={Homepage} />
+              </Switch>
+            </div>
+            <footer className="m-0 p-0">
+              <div className="container py-4 text-center text-uppercase">
+                <Link to="/">Home</Link> ·{" "}
+                <Link to="/location">Add a store</Link> ·{" "}
+                <Link to="/about">About</Link>
+              </div>
+            </footer>
+          </div>
+        </AppContext.Provider>
+      </Router>
+    );
+  }
 }
 
-function App() {
-  return (
-    <Router history={history}>
-      <div className="App">
-        <ScrollToTop />
-        <AppNavbar />
-        <div className="page">
-          <Switch>
-            <Route path="/update" component={SubmitPage} />
-            <Route path="/location" component={LocationSelectionPage} />
-            <Route path="/about" component={AboutPage} />
-            <Route path="/" component={Homepage} />
-          </Switch>
-        </div>
-        <footer className="m-0 p-0">
-          <div className="container py-4 text-center text-uppercase">
-            <Link to="/">Home</Link> · <Link to="/location">Add a store</Link> ·{" "}
-            <Link to="/about">About</Link>
-          </div>
-        </footer>
-      </div>
-    </Router>
-  );
+export function withGlobalContext(Component) {
+  return function WrapperComponent(props) {
+    return (
+      <AppContext.Consumer>
+        {context => <Component {...props} {...context} />}
+      </AppContext.Consumer>
+    );
+  };
 }
 
 export default App;

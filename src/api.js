ae94d1a763238687dae7c75344e17bdee212f6d0
Arjun Attam <arjunattam@gmail.com>
2020-04-08 11:30:41 -0700

    move to v1

--- src/api.js
+++ src/api.js
@@ -4,12 +4,12 @@ const BASE_URL = "https://toilet-paper-app.herokuapp.com";
 // const BASE_URL = "http://localhost:5000";
 
 export async function query() {
-  const response = await axios.get(`${BASE_URL}/v0/query`);
+  const response = await axios.get(`${BASE_URL}/v1/query`);
   return response.data;
 }
 
 export async function submit(data) {
-  const response = await axios.post(`${BASE_URL}/v0/update`, data);
+  const response = await axios.post(`${BASE_URL}/v1/update`, data);
   return response.data;
 }
 

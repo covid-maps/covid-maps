68f5f45e39bf45efc9cc072308f8aad9e1efbb62
Arjun Attam <arjunattam@gmail.com>
2020-04-12 20:18:28 -0700

    server log to check ci

--- server/index.js
+++ server/index.js
@@ -86,4 +86,4 @@ app.get("/v2/query", async (req, res) => {
 
 const port = process.env.PORT || 5000
 app.listen(port);
-console.log('Server listening at port', port)
+console.log('Server is now listening at port', port)

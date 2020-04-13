fdf8152cc65e54fc1da652485ba6f3b063317a4e
siddharth11 <siddharth2parmar@gmail.com>
2020-04-13 03:56:06 +0200

    Abstract out HomepageAlerts

--- src/Homepage/index.js
+++ src/Homepage/index.js
@@ -3,8 +3,6 @@ import qs from "qs";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import PropTypes from "prop-types";
-import Alert from "react-bootstrap/Alert";
-import Snackbar from "@material-ui/core/Snackbar";
 import SearchResults from "./SearchResults";
 import MissingBlock from "./MissingBlock";
 import * as api from "../api";
@@ -13,8 +11,8 @@ import HomepageMapWithSearch from "../MapsWithSearch/HomepageMap";
 import { isStoreType, getFirstComma } from "../utils";
 import { recordAddNewStore, recordStoreFilterKeypress } from "../gaEvents";
 import Form from "react-bootstrap/Form";
-import ShareButton from "../ShareButton";
 import { withGlobalContext } from "../App";
+import HomepageAlerts from "./HomepageAlerts";
 import { FORM_FIELDS, ALERTS_TYPE } from "../constants";
 
 const DISTANCE_FILTER = 200000; // meters
@@ -272,56 +270,6 @@ class Homepage extends React.Component {
     }
   };
 
-  getAlertContent = () => {
-    const { translations } = this.props;
-    switch (this.state.alertType) {
-      case ALERTS_TYPE.WEBSITE_PURPOSE:
-        return (
-          <Alert
-            key="no-of-users"
-            className="card no-of-users-alert"
-            variant="primary"
-            show={this.state.showAlert}
-            onClose={this.toggleAlert}
-            dismissible
-          >
-            {translations.website_purpose_banner}
-          </Alert>
-        );
-      case ALERTS_TYPE.FORM_SUBMIT_SUCESS:
-        return (
-          <Snackbar
-            anchorOrigin={{ vertical: "top", horizontal: "center" }}
-            open={this.state.showAlert}
-            onClose={this.toggleAlert}
-          >
-            <Alert
-              className="mb-0"
-              show={this.state.showAlert}
-              key="form-submit-success"
-              variant="success"
-              onClose={this.toggleAlert}
-              dismissible
-            >
-              {translations.form_submit_success}
-              <ShareButton
-                className="d-block mt-2"
-                variant="success"
-                size="sm"
-                title="Covid Maps"
-                url="https://covidmaps.in/"
-                text={translations.website_share_description}
-              >
-                {translations.share_app}
-              </ShareButton>
-            </Alert>
-          </Snackbar>
-        );
-      default:
-        return null;
-    }
-  };
-
   render() {
     let missingBlock = null;
     let selectedForMissing = undefined;
@@ -345,7 +293,11 @@ class Homepage extends React.Component {
     const { translations } = this.props;
     return (
       <div>
-        {this.getAlertContent()}
+        <HomepageAlerts
+          {...this.props}
+          showAlert={this.showAlert}
+          toggleAlert={this.toggleAlert}
+        />
         <HomepageMapWithSearch
           value=""
           onSearchSuccess={this.onSearchCompleted}

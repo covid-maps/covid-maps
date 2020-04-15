86e9f5e225fd3f57df70e7692a240eb83404b2b2
Arjun Attam <arjunattam@gmail.com>
2020-04-14 22:23:47 -0700

    search GA events send type for aggregation

--- src/MapsWithSearch/Input/index.js
+++ src/MapsWithSearch/Input/index.js
@@ -73,13 +73,13 @@ class LocationSearchInput extends React.Component {
 
   handleSelect = address => {
     this.textInput.current.blur();
-    recordSearchCompleted();
     geocodeByAddress(address)
       .then(async results => {
         this.setState({ address });
         const result = results[0];
         const latLng = await getLatLng(result);
         console.log("Success", latLng);
+        recordSearchCompleted(result.types);
         this.props.onSearchSuccess({
           latLng: latLng,
           name: address,

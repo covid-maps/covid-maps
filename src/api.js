411034f9f3df5c559887e93ebca882b980d4f672
Siddharth11 <siddharth2parmar@gmail.com>
2020-04-17 09:38:48 +0200

    Use BASE_URL from process.env

--- src/api.js
+++ src/api.js
@@ -1,17 +1,13 @@
 import axios from "axios";
 
-const BASE_URLS = {
-  PROD: "https://toilet-paper-app.herokuapp.com",
-  STAGING: "http://covid-maps-staging.herokuapp.com",
-  LOCAL: "http://localhost:5000"
-}
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export async function query(params) {
-  const response = await axios.get(`${BASE_URLS.PROD}/v2/query`, { params });
+  const response = await axios.get(`${BASE_URL}/v2/query`, { params });
   return response.data;
 }
 
 export async function submit(data) {
-  const response = await axios.post(`${BASE_URLS.PROD}/v1/update`, data);
+  const response = await axios.post(`${BASE_URL}/v1/update`, data);
   return response.data;
 }

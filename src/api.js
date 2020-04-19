3326289a4b4b009cb6a380f6cb222df31dcb3aba
Preet Parekh <preetjdp123@gmail.com>
2020-04-19 13:33:49 +0530

    Frontend Changes

--- src/api.js
+++ src/api.js
@@ -3,9 +3,13 @@ import axios from "axios";
 // coming from .env files
 // you can setup a new .env.local file to override these vars locally
 const BASE_URL = process.env.REACT_APP_BASE_URL;
-
 export async function query(params) {
   const response = await axios.get(`${BASE_URL}/v2/query`, { params });
+  return response.data;
+}
+
+export async function queryByStoreId(params) {
+  const response = await axios.get(`${BASE_URL}/v2/queryByStoreId`, { params });
   return response.data;
 }
 

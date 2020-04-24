fd6513e87d0e5b5706ef082389f97adc2ae19096
Siddharth11 <siddharth2parmar@gmail.com>
2020-04-24 17:20:49 +0200

    Do not keep font size too small

--- src/Homepage/EntriesGroup.js
+++ src/Homepage/EntriesGroup.js
@@ -103,11 +103,9 @@ const SingleEntry = ({ entry, highlightedText, translations }) => {
           </div>
         ) : null}
       </div>
-      <div style={{ fontSize: "0.85em" }}>
-        <small className="text-muted d-inline-block mt-2">
-          Updated <Timestamp {...entry} />
-        </small>
-      </div>
+      <small className="text-muted d-block mt-2">
+        Updated <Timestamp {...entry} />
+      </small>
     </div>
   );
 };

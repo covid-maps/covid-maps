1ca2279dbb675f4e433b0544ce58b0bae92938af
Arjun Attam <arjunattam@gmail.com>
2020-04-27 23:58:57 -0700

    migrate to typescript

--- server/src/service/votes.ts
+++ server/src/service/votes.ts
@@ -1,11 +1,7 @@
 const assert = require("assert");
 const models = require("../models");
 
-async function addVote(data) {
+export async function addVote(data) {
   assert(data.type === "up" || data.type === "down");
   return await models.Votes.create(data);
 }
-
-module.exports = {
-  addVote,
-};
